c3134bae30c0a1e128644aba74104ec38f0d3e91
jeremy <jsheather17@gmail.com>
2020-10-09 21:35:45 +1000

    Image converting script

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # egh444-group-project
+
+## Image download and convert scripts
+
+To setup the Python environment:
+
+```
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
+```
+
+To download external images of sydney and paris, and then get the 'other' images (images require manual sorting after this process):
+```
+python download_images.py
+```
+
+To convert jpgs to png once downloaded and sorted:
+```
+python convert_images.py <root_directory>
+```
---
+++ convert_images.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import threading
+import time
+
+from PIL import Image
+
+def convert_image(input_file):
+    '''
+    Convert provided image from jpg to png
+    '''
+    name, ext = os.path.splitext(f)
+    location, filename = os.path.split(input_file)
+    print("Converting {} to {}".format(input_file, os.path.join(location, "{}.png".format(name))))
+    im = Image.open(input_file)
+    im.convert('RGB').save(os.path.join(location, "{}.png".format(name)))
+
+if __name__ == "__main__":
+    try:
+        root = sys.argv[1]
+    except IndexError:
+        print("Error: must provide root directory as first argument. ")
+        sys.exit(1)
+
+    # Cycle through files
+    for subdir, dirs, files in os.walk(os.path.join(os.getcwd(), root)):
+        for f in files:
+            # Get the file extension
+            name, ext = os.path.splitext(f)
+            # Is it a png?
+            if ext in ('.jpg', '.jpeg'):
+                # Wait for threads to be available (max 8)
+                # Create thread
+                while (threading.active_count() > 8):
+                    time.sleep(1)
+                threading.Thread(target=convert_image, args=(os.path.join(subdir, f),)).start()
