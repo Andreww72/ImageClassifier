12de506b5bb9f299355f29ee3eb46f4e7e27f4c2
jeremy <jsheather17@gmail.com>
2020-10-15 23:19:31 +1000

    Pipeline working and ready to go

--- detect_landmark.m
+++ detect_landmark.m
@@ -1,3 +1,28 @@
 function landmark = detect_landmark(img)
-% Do expected work in here
+    im_width = 224;
+    im_height = 224;
+    
+    load resnet50_11-11-20.mat net
+    
+    R = img(:,:,1);
+    G = img(:,:,2);
+    B = img(:,:,3);
+    
+    Rr = imresize(R,[im_width,im_height]);
+    Gr = imresize(G,[im_width,im_height]);
+    Br = imresize(B,[im_width,im_height]);
+    
+    img_resized = cat(3, Rr, Gr, Br);
+    
+    [~, scores] = classify(net, img_resized);
+    
+    [~, idx] = max(scores);
+    
+    if idx == 1
+        landmark = 0;
+    elseif idx == 2
+        landmark = 2;
+    elseif idx == 3
+        landmark = 1;
+    end
 end
