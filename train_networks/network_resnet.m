5338cfce04f352871af4b57129af74d866fbbd01
Andrew Mather <andreww.mather@gmail.com>
2020-10-17 17:09:10 +1000

    ResNet-18, also ignore where I saved my thicc bois

--- train_networks/network_resnet.m
+++ train_networks/network_resnet.m
@@ -1,29 +1,33 @@
-% ResNet-50
-% https://au.mathworks.com/help/deeplearning/ref/resnet50.html
+%% ResNet-18
+% https://au.mathworks.com/help/deeplearning/ref/resnet18.html
 % https://au.mathworks.com/help/deeplearning/ug/train-deep-learning-network-to-classify-new-images.html
-image_directory = "../EGH444_Project_2020_TrainingData/";
+
+%% Setup
+image_directory = "../Images/";
 imds = imageDatastore(image_directory, ...
     'IncludeSubfolders',true, ...
     'LabelSource','foldernames');
 imds.ReadSize = numpartitions(imds);
 imds.ReadFcn = @(loc)imresize(imread(loc),[300,300]);
 imds.ReadSize = numpartitions(imds);
-[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7);
 
-net = resnet50;
+% Split into training and validation 70-30
+[imdsTrain,imdsValidation] = splitEachLabel(imds,0.7);
+numClasses = numel(categories(imdsTrain.Labels));
+
+%% Transfer learning
+net = resnet18;
 analyzeNetwork(net)
 net.Layers(1)
 inputSize = net.Layers(1).InputSize;
 
+% Replace final layers
 if isa(net,'SeriesNetwork') 
   lgraph = layerGraph(net.Layers); 
 else
   lgraph = layerGraph(net);
 end
 [learnableLayer,classLayer] = findLayersToReplace(lgraph);
-[learnableLayer,classLayer]
-
-numClasses = numel(categories(imdsTrain.Labels));
 
 if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
     newLearnableLayer = fullyConnectedLayer(numClasses, ...
@@ -43,16 +47,20 @@ lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
 newClassLayer = classificationLayer('Name','new_classoutput');
 lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
 
+% Check network reconstructed correctly
 figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
 plot(lgraph)
 ylim([0,10])
 
+% Important variables
 layers = lgraph.Layers;
 connections = lgraph.Connections;
 
-layers(1:10) = freezeWeights(layers(1:10));
+% Freeze initial layers (layers in initial stem)
+layers(1:4) = freezeWeights(layers(1:4));
 lgraph = createLgraphUsingConnections(layers,connections);
 
+%% Train network
 pixelRange = [-30 30];
 scaleRange = [0.9 1.1];
 imageAugmenter = imageDataAugmenter( ...
@@ -61,7 +69,8 @@ imageAugmenter = imageDataAugmenter( ...
     'RandYTranslation',pixelRange, ...
     'RandXScale',scaleRange, ...
     'RandYScale',scaleRange);
-augimdsTrain = augmentedImageDatastore(inputSize(1:2),imdsTrain, ...
+
+augimdsTrain = augmentedImageDatastore(inputSize(1:2), imdsTrain, ...
     'DataAugmentation',imageAugmenter);
 
 augimdsValidation = augmentedImageDatastore(inputSize(1:2),imdsValidation);
@@ -81,6 +90,7 @@ options = trainingOptions('sgdm', ...
 
 net = trainNetwork(augimdsTrain,lgraph,options);
 
+%% Classify validation images
 [YPred,probs] = classify(net,augimdsValidation);
 accuracy = mean(YPred == imdsValidation.Labels)
 
