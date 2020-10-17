ed5a7f9f978c9d7f4aca11cd02895a2af4b94eea
Andrew Mather <andreww.mather@gmail.com>
2020-10-17 17:55:06 +1000

    Lachlan's magic find baddies code

---
+++ train_networks/findBadImageFormats.m
@@ -0,0 +1,23 @@
+% Find bad image formats
+
+image_directory = "../Images/";
+imds = imageDatastore(image_directory, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource','foldernames');
+imds.ReadSize = numpartitions(imds);
+imds.ReadFcn = @(loc)imresize(imread(loc),[300,300]);
+imds.ReadSize = numpartitions(imds);
+
+fileNames = [string(imds.Files)];
+badFiles = [];
+for i = 1:length(fileNames)
+   fileName = fileNames(i);
+   try
+       test = imread(fileName);
+       if (size(test,3) ~= 3)
+           badFiles = [badFiles fileName];
+       end
+   catch
+       badFiles = [badFiles fileName];
+   end
+end
