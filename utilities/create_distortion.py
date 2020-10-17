cc1fcf6b4da67294518ec724b5e38f04828f01ed
jeremy <jsheather17@gmail.com>
2020-10-17 12:55:49 +1000

    Distortion script complete

--- utilities/create_distortion.py
+++ utilities/create_distortion.py
@@ -1,10 +1,55 @@
 import os
 import random
 import sys
+import threading
+import time
 
 from wand.image import Image
+from wand.color import Color
 import numpy as np
 import cv2 
+
+def distort_img(input_file, output_dir):
+    print("Distorting {}...".format(input_file))
+    with Image(filename=input_file) as img:
+        # Set virtual pixels to transparent
+        img.virtual_pixel = 'transparent'
+
+        # Create parameters for distortion
+        a = random.uniform(0, 1)
+        b = random.uniform(2, 5)
+        c = random.uniform(6, 10)
+        d = random.uniform(0, 10)
+        total = a + b + c + d
+        a = a / total
+        b = b / total
+        c = c / total
+        d = d / total
+        # Distort image
+        img.distort('barrel', (a, b, c, d))
+
+        # Crop out unnecessary transparent pixels
+        # Start by finding which axis we're working in
+        min_axis_size = min(img.size)
+        if min_axis_size == img.size[0]:
+            col_to_look = min_axis_size/2
+            row_idx = 0
+            while img[int(col_to_look), int(row_idx)] == Color(string='transparent'):
+                row_idx += 1
+            img.crop(0, row_idx, img.size[0], (img.size[1]-row_idx))
+        elif min_axis_size == img.size[1]:
+            row_to_look = min_axis_size/2
+            col_idx = 0
+            while img[int(col_idx), int(row_to_look)] == Color(string='transparent'):
+                col_idx += 1
+            img.crop(col_idx, 0, (img.size[0]-col_idx), img.size[1])
+        
+        location, filename = os.path.split(input_file)
+        name, ext = os.path.splitext(filename)
+        path, parent = os.path.split(location)
+        os.makedirs(os.path.join(output_dir, parent), exist_ok=True)
+
+        img.save(filename=os.path.join(output_dir, parent, "{}_distorted{}".format(name, ext)))
 
 if __name__ == "__main__":
     try:
@@ -15,17 +60,16 @@ if __name__ == "__main__":
         sys.exit(1)
 
     random.seed(1)
+    image = None
+
+    # Make output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
 
     for subdir, dirs, files in os.walk(input_dir):
         for f in files:
             name, ext = os.path.splitext(f)
             if ext == ".png":
                 # Open the image with wand
-                with Image(filename=os.path.join(subdir, f)) as img:
-                    img.virtual_pixel = 'transparent' #? ngl
-                    a = random.uniform(0, 1)
-                    b = random.uniform(0, 1)
-                    c = random.uniform(0, 1)
-                    d = random.uniform(0, 1)
-                    img.distort('barrel', (a, b, c, d))
-                    img.save(filename='test.png')
+                while (threading.active_count() > 8):
+                    time.sleep(1)
+                threading.Thread(target=distort_img, args=(os.path.join(subdir,f), output_dir,)).start()
